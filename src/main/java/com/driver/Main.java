a41a06f3a33886172ff7fbf480f4e5207cc9c372
gundasriharsha <harshagunda2002@gmail.com>
2023-05-12 11:47:20 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void Main() {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Sriharsha");
+        String name = rwOnly.getName();
+    }
+
+
 }
